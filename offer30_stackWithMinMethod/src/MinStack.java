9eaf085ad8ea67b97e85b4b31785c693f738f2cc
Yansufeng <392576297@qq.com>
2021-10-29 22:00:23 +0800

    init

---
+++ offer30_stackWithMinMethod/src/MinStack.java
@@ -0,0 +1,29 @@
+/**
+ * @Author Yansufeng
+ * @Date 2021/10/29 9:57 下午
+ */
+public class MinStack {
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return 0;
+    }
+
+    public int min() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
