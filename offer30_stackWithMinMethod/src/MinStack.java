8a1156ba767f4a35d5dabdc23f60667ab6b509c0
Yansufeng <392576297@qq.com>
2021-10-30 13:10:38 +0800

    使用辅助栈选择性压入当前最小值

--- offer30_stackWithMinMethod/src/MinStack.java
+++ offer30_stackWithMinMethod/src/MinStack.java
@@ -1,29 +1,51 @@
+import java.util.Stack;
+
 /**
  * @Author Yansufeng
  * @Date 2021/10/29 9:57 下午
  */
 public class MinStack {
-    public MinStack() {
+    private final Stack<Integer> stack;
+    private final Stack<Integer> stackForMin;
 
+    public MinStack() {
+        stack = new Stack<>();
+        stackForMin = new Stack<>();
     }
 
     public void push(int x) {
+        stack.push(x);
 
+        if(!stackForMin.empty())
+            if(stackForMin.peek() < x)
+                return;
+
+        stackForMin.push(x);
     }
 
     public void pop() {
+        int top = stack.pop();
 
+        if(stackForMin.peek() == top)
+            stackForMin.pop();
     }
 
     public int top() {
-        return 0;
+        return stack.peek();
     }
 
     public int min() {
-        return 0;
+        return stackForMin.peek();
     }
 
     public static void main(String[] args) {
+        MinStack m = new MinStack();
 
+        m.push(0);
+        m.push(1);
+        m.push(0);
+        System.out.println(m.min());
+        m.pop();
+        System.out.println(m.min());
     }
 }
