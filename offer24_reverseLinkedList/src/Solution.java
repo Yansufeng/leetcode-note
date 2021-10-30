6d2d60845f9d86496a1f8a12900ba641ab60351a
Yansufeng <392576297@qq.com>
2021-10-30 21:43:53 +0800

    init

---
+++ offer24_reverseLinkedList/src/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * @Author Yansufeng
+ * @Date 2021/10/30 9:42 下午
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        return new ListNode(0);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
